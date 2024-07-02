d7dffc25a82ac89a0aa5233007c04245fda08f6d
Abdul <malik301002@gmail.com>
2024-07-03 02:12:46 +0700

    refactor(app): remove posts layout

--- src/app/posts/loading.tsx
+++ src/app/posts/loading.tsx
@@ -2,10 +2,10 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function Loading() {
   return (
-    <section className="my-8 flex flex-col gap-4">
+    <main className="my-8 flex flex-col gap-4">
       {[1, 2, 3].map(num => (
         <Skeleton key={num} className="h-[170px] w-full" />
       ))}
-    </section>
+    </main>
   );
 }
