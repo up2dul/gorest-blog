7f7b014c7d59325bddec4c3617bd135835e5c7b3
Abdul <malik301002@gmail.com>
2024-07-02 08:56:51 +0700

    feat: pagination for post list

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,18 +1,30 @@
+import { toInt } from 'radash';
+
 import { PostCard } from '@/components/common/content-card';
+import { Pagination } from '@/components/common/pagination';
 import { getPosts } from '@/lib/api';
 
-export default async function Posts() {
-  const posts = await getPosts();
+type PostProps = {
+  searchParams: {
+    page: string;
+  };
+};
+
+export default async function Posts({ searchParams }: PostProps) {
+  const page = toInt(searchParams.page);
+  const posts = await getPosts(page);
 
   return (
     <main>
       <h1 className="text-3xl font-bold">📝 Blog Post List</h1>
 
-      <section className="mt-8 flex flex-col gap-4">
+      <section className="my-8 flex flex-col gap-4">
         {posts.map(post => (
           <PostCard key={post.id} post={post} />
         ))}
       </section>
+
+      <Pagination />
     </main>
   );
 }
