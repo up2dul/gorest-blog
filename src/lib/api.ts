f65c6aa41cbc0bcba6fe41f7bb812ff747c32e8f
Abdul <malik301002@gmail.com>
2024-07-03 08:24:06 +0700

    feat(api): create users fetcher func

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,6 +3,7 @@ import { tryit } from 'radash';
 import type { Comment, Post, User } from './types';
 
 const BASEURL = 'https://gorest.co.in/public/v2';
+const PERPAGE = 10;
 
 async function fetchData<T>(url: string): Promise<T> {
   const response = await fetch(url);
@@ -10,7 +11,9 @@ async function fetchData<T>(url: string): Promise<T> {
 }
 
 async function fetchPosts(page: number): Promise<Post[]> {
-  return await fetchData<Post[]>(`${BASEURL}/posts?per_page=10&page=${page}`);
+  return await fetchData<Post[]>(
+    `${BASEURL}/posts?per_page=${PERPAGE}&page=${page}`,
+  );
 }
 
 async function fetchPostDetail(postId: number): Promise<Post> {
@@ -21,6 +24,15 @@ async function fetchPostComments(postId: number): Promise<Comment[]> {
   return await fetchData<Comment[]>(`${BASEURL}/posts/${postId}/comments`);
 }
 
+async function fetchUsers({
+  page,
+  nameSearch,
+}: { page: number; nameSearch: string }) {
+  return await fetchData<User[]>(
+    `${BASEURL}/users?per_page=${PERPAGE}&page=${page}&name=${nameSearch}`,
+  );
+}
+
 async function fetchUserDetail(userId: number): Promise<User> {
   return await fetchData<User>(`${BASEURL}/users/${userId}`);
 }
@@ -28,4 +40,5 @@ async function fetchUserDetail(userId: number): Promise<User> {
 export const getPosts = tryit(fetchPosts);
 export const getPostDetail = tryit(fetchPostDetail);
 export const getPostComments = tryit(fetchPostComments);
+export const getUsers = tryit(fetchUsers);
 export const getUserDetail = tryit(fetchUserDetail);
