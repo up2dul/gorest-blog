99cf6a64a75610f82a3cb2c89e59c79c8bad77e1
abdul <up2dul@gmail.com>
2025-03-09 17:07:51 +0700

    feat: create and adjust layout with header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,26 @@
 import { useQueryPosts } from "@/hooks/use-posts";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Card, Col, Row, Spin } from "antd";
 
 export default function Home() {
   const { data: posts, isLoading, isError, error } = useQueryPosts();
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Spin size="large" className="mt-20 w-full" />;
   }
 
   if (isError) {
-    return <div>Error: {JSON.stringify(error.message, null, 2)}</div>;
+    return <h1>Error: {error.message}</h1>;
   }
 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <pre>{JSON.stringify(posts, null, 2)}</pre>
-    </main>
+    <Row gutter={[12, 12]}>
+      {posts?.data.map(post => (
+        <Col key={post.id} className="gutter-row" md={12}>
+          <Card title={post.title} className="w-full">
+            <p className="line-clamp-2">{post.body}</p>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   );
 }
