5831003e2ea6567e72bdca11471b44654995eb6d
abdul <up2dul@gmail.com>
2025-03-09 17:06:23 +0700

    fix(pages): warning on meta title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
+import { MainLayout } from "@/components/layout/main-layout";
 import { AntdConfigProvider } from "@/context/antd-config-context";
 import { AuthProvider } from "@/context/auth-context";
-import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+
+import "@/styles/globals.css";
+import Head from "next/head";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,7 +21,13 @@ export default function App({ Component, pageProps }: AppProps) {
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
         <AntdConfigProvider>
-          <Component {...pageProps} />
+          <Head>
+            <title>Gorest Blog Posts</title>
+            <meta name="description" content="Blog posts from Gorest API" />
+          </Head>
+          <MainLayout>
+            <Component {...pageProps} />
+          </MainLayout>
         </AntdConfigProvider>
       </QueryClientProvider>
     </AuthProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Gorest Blog Posts</title>
-        <meta name="description" content="Blog posts from Gorest API" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
