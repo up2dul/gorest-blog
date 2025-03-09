4eb8d0026ab8fecf8423086902e4662d26cd11fc
abdul <up2dul@gmail.com>
2025-03-09 17:08:58 +0700

    feat: generic type on API getter and add URL params

--- src/hooks/use-posts.ts
+++ src/hooks/use-posts.ts
@@ -1,10 +1,11 @@
 import { POSTS_KEY } from "@/lib/constants";
+import type { Post } from "@/lib/types";
 import apiClient from "@/services/api-client";
 import { useQuery } from "@tanstack/react-query";
 
-export function useQueryPosts() {
+export function useQueryPosts({ page = 1 }: { page?: number } = { page: 1 }) {
   return useQuery({
     queryKey: [POSTS_KEY],
-    queryFn: () => apiClient.get("/"),
+    queryFn: () => apiClient.get<Post[]>(`?per_page=10&page=${page}`),
   });
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,3 +2,10 @@ export type UserAuth = {
   name: string;
   token: string;
 };
+
+export type Post = {
+  id: number;
+  user_id: number;
+  title: string;
+  body: string;
+};
