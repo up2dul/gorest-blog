0676d25db2f7074fa1e95ff09a6c39532d02655c
abdul <up2dul@gmail.com>
2025-03-11 02:24:32 +0700

    feat(context): remove unused vars

--- src/context/antd-config-context.tsx
+++ src/context/antd-config-context.tsx
@@ -1,4 +1,4 @@
-import { ConfigProvider, message, theme } from "antd";
+import { ConfigProvider, theme } from "antd";
 import {
   type ReactNode,
   createContext,
@@ -15,7 +15,6 @@ type AntdConfigContextType = {
 const AntdConfigContext = createContext<AntdConfigContextType | null>(null);
 
 export const AntdConfigProvider = ({ children }: { children: ReactNode }) => {
-  const [contextHolder] = message.useMessage();
   const { defaultAlgorithm, darkAlgorithm } = theme;
   const storedTheme =
     localStorage.getItem("theme") === "dark" ? "dark" : "light";
