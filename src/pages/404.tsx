132bdfc61b330a0a53d1eec47bd7028bd5baf080
abdul <up2dul@gmail.com>
2025-03-11 13:33:21 +0700

    feat(pages): custom 404 and 500 pages
    Closes: #17

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <section>
+      <h1 className="mt-4 text-2xl text-balance text-center md:text-3xl">
+        404 - Page not found
+      </h1>
+    </section>
+  );
+}
---
+++ src/pages/500.tsx
@@ -0,0 +1,9 @@
+export default function ServerError() {
+  return (
+    <section>
+      <h1 className="mt-4 text-2xl text-balance text-center md:text-3xl">
+        500 - Server-side error occurred
+      </h1>
+    </section>
+  );
+}
